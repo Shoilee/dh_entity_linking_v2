b07791dd2d4d2f3e25aa797f9ef4fde7d8711b8c
Shoilee <sarahshoilee121@gmail.com>
2023-10-02 10:17:49 +0200

    add results from manual evaluation

--- matchfuzzystring/match_fuzzy_string.py
+++ matchfuzzystring/match_fuzzy_string.py
@@ -8,20 +8,21 @@ def match_fuzzy_string(df1, df2, ratio=fuzz.partial_token_sort_ratio, max_score=
     # print(f"Shape of NMVW constinuent: {df1.shape}")
     # print(f"Shape of Bronbeek constinuent: {df2.shape}")
 
-    list_of_names = [str(row['name_label']) for i,row in df1.iterrows()]
+    # filter out names that have 4 or less character in it
+    list_of_names = [str(row['name_label']) for i, row in df1.iterrows() if len(str(row['name_label'])) > 4]
 
     match = ["NO" for i in range(len(df2.index))]
     match_results = []
 
     for i, row in tqdm(df2.iterrows()):
         row_match_results = list()
-        row_match_results = process.extractBests(str(row['DisplayName']), list_of_names,
+        row_match_results = process.extractBests(str(row['FullName']), list_of_names,
                                                  scorer=ratio, score_cutoff=max_score, limit=candidate_size)
         if len(row_match_results) > 0:
             match[i] = "YES"
         match_results.append(row_match_results)
 
-    temp_df = pandas.DataFrame({'RetrievedNames': match_results, 'MATCH':match})
+    temp_df = pandas.DataFrame({'RetrievedNames': match_results, 'MATCH': match})
     result_table = pandas.concat([df2, temp_df], axis=1)
     print(result_table[['DisplayName', 'RetrievedNames', 'MATCH']].head(10))
 
