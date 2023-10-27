f6c8789c2ca2ce5f5c817307ba4fc82411809b52
Shoilee <sarahshoilee121@gmail.com>
2023-10-27 11:18:56 +0200

    update code with correspondance matching task and parellel processing

--- matchwithabbreviation/match_with_abbreviation.py
+++ matchwithabbreviation/match_with_abbreviation.py
@@ -1,5 +1,34 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas
+import numpy as np
+import multiprocessing as mp
 from tqdm import tqdm
+
+def get_initials(name):
+    if len(name.split(" ")) < 1:
+        print(name)
+        return
+
+    names = name.split(' ')
+    try:
+        return ' '.join([f"{n[0]}." for n in names])
+    except Exception as e:
+        # print(f'{name} has {str(e)} error')
+        pass
+
+
+def get_abbreviation(fullname):
+    # checking if fullname is nan
+    if type(fullname) == float and np.isnan(fullname):
+        return
+    firstnames = get_initials(" ".join(str(fullname).split(" ")[:-1]))
+    lastname = fullname.split(" ")[-1]
+    if firstnames:
+        initial_and_surname = firstnames + " " + lastname
+    else:
+        initial_and_surname = lastname
+    return initial_and_surname
 
 
 def match_with_abbreviation(df1, df2):
@@ -10,7 +39,7 @@ def match_with_abbreviation(df1, df2):
         It generates abbreviated versions of names in both DataFrames and then merges them using the 'Abbreviations' column.
 
         Args:
-            df1 (pandas.DataFrame): The first DataFrame containing 'name_label' and other relevant columns.
+            df1 (pandas.DataFrame): The first DataFrame containing 'pref_label' and other relevant columns.
             df2 (pandas.DataFrame): The second DataFrame containing 'FirstName', 'LastName', and other relevant columns.
 
         Returns:
@@ -28,7 +57,7 @@ def match_with_abbreviation(df1, df2):
         Example:
         df1:
         +----+--------------+---------------+
-        |    |  name_label |  ...          |
+        |    |  pref_label |  ...          |
         +----+--------------+---------------+
         |  0 | John Doe     | ...           |
         |  1 | Jane Smith   | ...           |
@@ -44,39 +73,30 @@ def match_with_abbreviation(df1, df2):
 
         match_with_abbreviation(df1, df2) returns:
         +----+--------------+--------------+---------------+--------------+---------------+
-        |    |  name_label | Abbreviations|  FirstName    | LastName     | RetrievedNames|
+        |    |  pref_label | Abbreviations|  FirstName    | LastName     | RetrievedNames|
         +----+--------------+--------------+---------------+--------------+---------------+
         |  0 | John Doe     | J. Doe       | John          | Johnson      | [John Doe]    |
         |  1 | Jane Smith   | J. Smith     | Mary          | Smith        | []            |
         +----+--------------+--------------+---------------+--------------+---------------+
         """
 
-    def get_initials(name):
-        if len(name.split(" ")) < 1:
-            print(name)
-            return
 
-        names = name.split(' ')
-        try:
-            return ' '.join([f"{n[0]}." for n in names])
-        except Exception as e:
-            # print(f'{name} has {str(e)} error')
-            pass
-
+    """
+    
     abbreviation_list = list()
     for i, row in df1.iterrows():
-        firstnames = get_initials(" ".join(str(row['name_label']).split(" ")[:-1]))
+        firstnames = get_initials(" ".join(str(row['pref_label']).split(" ")[:-1]))
         if firstnames:
-            initial_and_surname = firstnames + " " + str(row['name_label']).split(" ")[-1]
+            initial_and_surname = firstnames + " " + str(row['pref_label']).split(" ")[-1]
         else:
-            initial_and_surname = str(row['name_label']).split(" ")[-1]
-        # print(f"{str(row['name_label'])} --> {initial_and_surname}")
+            initial_and_surname = str(row['pref_label']).split(" ")[-1]
+        # print(f"{str(row['pref_label'])} --> {initial_and_surname}")
         abbreviation_list.append(initial_and_surname)
 
     temp_df = pandas.DataFrame({'Abbreviations': abbreviation_list})
     df1 = pandas.concat([df1, temp_df], axis=1)
 
-    # print(df1[['name_label', 'Abbreviations']])
+    # print(df1[['pref_label', 'Abbreviations']])
 
     # print("#######################################################################")
 
@@ -94,7 +114,7 @@ def match_with_abbreviation(df1, df2):
     df2 = pandas.concat([df2, temp_df], axis=1)
 
     # print(df2[['FirstName', 'LastName', 'Abbreviations']])
-
+    """
     """
     match = ["NO" for i in range(len(df2.index))]
     match_results = []
@@ -102,7 +122,7 @@ def match_with_abbreviation(df1, df2):
         row_match_results = list()
         for j, row_df1 in df1.iterrows():
             if str(row_df2['Abbreviations']) == str(row_df1['Abbreviations']):
-                row_match_results.append(str(row_df1['name_label']))
+                row_match_results.append(str(row_df1['pref_label']))
                 match[i] = "YES"
         match_results.append(row_match_results)
 
@@ -110,8 +130,14 @@ def match_with_abbreviation(df1, df2):
     result_table = pandas.concat([df2, temp_df], axis=1)
     # print(result_table[['FirstName', 'LastName', 'RetrievedNames', 'MATCH']])
     """
+    n_workers = int(mp.cpu_count() * 2)
+    print(f"{n_workers} workers are available")
+    pool = mp.Pool(n_workers)
+    df1['Abbreviation'] = pool.map(get_abbreviation, df1['pref_label'])
+
+    df2['Abbreviation'] = pool.map(get_abbreviation, df2['FullName'])
 
     # result_table = df2.join(df1, lsuffix="_left", rsuffix="_right")
-    result_table = pandas.merge(df1, df2, on='Abbreviations')
+    result_table = pandas.merge(df1, df2, on='Abbreviation')
     return result_table
 
