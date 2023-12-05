e4ece83fe2953fa7281c17f615619f74993021f2
Shoilee <sarahshoilee121@gmail.com>
2023-12-05 13:38:51 +0100

    update the calculate result code and adjust relavent file accordingly

--- exp202/calculate_result.py
+++ exp202/calculate_result.py
@@ -2,49 +2,70 @@ import pandas
 
 
 def recall(df, ground_truth):
+    all_positive = len(ground_truth)
     true_positive = 0
     for index, row in df.iterrows():
-        if row['ConstituentID'] in ground_truth.loc[ground_truth['nmvw_uri'] == row['nmvw_uri']]['ConstituentID'].values:
+        if row['ConstituentID'] in ground_truth.loc[ground_truth['nmvw_uri'] == str(row['nmvw_uri'])]['ConstituentID'].values:
+            # for https://hdl.handle.net/20.500.11840/pi64129 in
             true_positive += 1
 
-    r = true_positive/len(ground_truth)
+    r = true_positive/all_positive
 
-    print(f"Total correspondence: {len(ground_truth)} \nTrue positive count: {true_positive}")
+    print(f"All positive count: {all_positive} \nTrue positive count: {true_positive}")
     print(f"Recall: {r}")
     print(f"\n")
 
     return r, len(ground_truth)
 
 
-# precision = number of correct correspondence / total retrieved
-# precision = true positive / (true positive + false positive)
+# p = number of correct correspondence / total retrieved
+# p = true positive / (true positive + false positive)
 def precision(df, ground_truth):
-    total_retrieved = len(df)
+    # total_retrieved = len(df)
+    false_positive = 0
     true_positive = 0
 
     for index, row in df.iterrows():
-        if row['ConstituentID'] in ground_truth.loc[ground_truth['nmvw_uri'] == row['nmvw_uri']]['ConstituentID'].values:
+        if ground_truth[ground_truth['nmvw_uri'] == str(row['nmvw_uri'])].empty:
+            continue # if the row['nmvw_uri'] do not match with any ground_truth['nmvw_uri'], DO NOTHING!
+        if row['ConstituentID'] in ground_truth.loc[ground_truth['nmvw_uri'] == str(row['nmvw_uri'])]['ConstituentID'].values:
             true_positive += 1
+        else:
+            print(row)
+            print("\n")
+            false_positive += 1
 
+        '''
+        for _, matched_row in ground_truth[ground_truth['nmvw_uri'] == str(row['nmvw_uri'])].iterrows():
+            if matched_row['ConstituentID'] == row['ConstituentID']:
+                true_positive += 1
+            else:
+                print(row)
+                false_positive += 1
+        '''
+
+    found_positive = true_positive + false_positive
     try:
-        p = true_positive / total_retrieved
+        p = true_positive / found_positive
     except ZeroDivisionError:
-        print(f"The total retrieved is: {total_retrieved}!!!!")
+        print(f"Found positive count: {found_positive}!!!!")
 
-    print(f"Total correspondence: {len(df)}\nTotal retrieved: {total_retrieved}\nTrue positive count: {true_positive} ")
+    print(f"Total true correspondence: {len(ground_truth)}\n"
+          f"Found positive count: {found_positive}\n"
+          f"True positive count: {true_positive} ")
     print(f"Precision: {p}")
     print(f"\n")
 
-    return p, true_positive, total_retrieved
+    return p, true_positive, found_positive
 
 
-def f_measure(recall, precision):
+def f_measure(r, p):
     try:
-        f = 2 * (precision * recall)/(precision + recall)
+        f = 2 * (p * r) / (p + r)
         print(f"F-measure: {f}\n")
         return f
     except ZeroDivisionError:
-        print(f"Both precision is {precision} and recall is {recall}!!!")
+        print(f"Both precision is {p} and recall is {r}!!!")
         return 0
 
 
