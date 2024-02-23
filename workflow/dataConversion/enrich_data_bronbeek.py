512f84c755d3def0841f120b3437e4aa444d3092
Shoilee <sarahshoilee121@gmail.com>
2024-02-23 12:40:31 +0100

    update script for enrich data

--- workflow/dataConversion/enrich_data_bronbeek.py
+++ workflow/dataConversion/enrich_data_bronbeek.py
@@ -15,29 +15,18 @@ def load_graph(directory):
 
     # Loop through each NQ file
     for root, dirs, files in os.walk(directory):
+        files = [f for f in files if f.endswith(".nq")]
         for file in files:
-            graphs.parse(os.path.join(root, file), format="nquads")
+            try:
+                graphs.parse(os.path.join(root, file), format="nquads")
+            except UnicodeDecodeError:
+                print(f"Error with: {os.path.join(root, file)}")
             print("\n======================\n")
             print(f"file: {os.path.join(root, file)}")
             print(f"Graph has "
                   f"{len([x for x in graphs.store.contexts()])} context/Named Graphs "
                   f"with {len(graphs.store)} triples")
-    """
-    
-    @param directory: 
-    @return: 
-    
-    store = Memory()
-    graphs = ConjunctiveGraph(store=store)
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            graphs = graphs.parse(os.path.join(root, file), format="nquads")
-            print("\n======================\n")
-            print(f"file: {os.path.join(root, file)}")
-            print(f"Graph has "
-                  f"{len([x for x in graphs.store.contexts()])} context/Named Graphs "
-                  f"with {len(graphs.store)} triples")
-    """
+
     return graphs
 
 
@@ -112,36 +101,112 @@ def add_acquisition_event(directory, graphs):
         .serialize(destination=os.path.join(directory, 'provenance_activity.ttl'))
 
 
-def add_former_owner():
+def add_former_owner(directory, graphs):
     def run_query(graph):
-        # TODO: write query that will inject triples
-        """
-        @return: table or pandas dataframe
+        query = """
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+
+            SELECT ?event ?constituent ?object
+            WHERE{
+                ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/ConstituentID> ?constituent .
+                ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/Prefix> ?event .
+                ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/RoleTypeID> ?RoleTypeID .
+                ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/ConXrefID> ?conxref .
+                ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/ID> ?ID .
+                ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/TableID> ?TableID .
+                FILTER (?TableID = "108"^^<https://pressingmatter.nl/Bronbeek/ConXrefs/interger>) .
+                ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/RoleTypeID> <https://pressingmatter.nl/Bronbeek/RoleTypes/2> .
+                ?object <https://pressingmatter.nl/Bronbeek/Objects/vocab/ObjectID> ?ID .
+            }
         """
 
-        q = """
-        SELECT * WHERE
-        {
-            ?s ?p ?o .
-        } LIMIT 10
-        """
-    pass
+        result = graph.query(query)
+
+        rows = []
+        for row in result:
+            event, constituent, object = row
+            rows.append({
+                'event': str(event),
+                'constituent': str(constituent),
+                'object': str(object)
+            })
+        df = pandas.DataFrame(rows)
+
+        return df
+
+    def insert_links(df):
+        g = Graph()
+        crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+        g.bind("crm", crm)
+
+        try:
+            for i, row in tqdm(df.iterrows()):
+                constituent = URIRef(str(row.iloc[1]).strip().replace('"', ''))
+                object = URIRef(str(row.iloc[2]).strip().replace('"', ''))
+
+                g.add((object, crm.P51_has_former_or_current_owner, constituent))
+        finally:
+            print(len(g))
+            return g
+
+    insert_links(run_query(graphs)) \
+        .serialize(destination=os.path.join(directory, 'former_owner.ttl'))
 
 
-def add_related_constituents():
+def add_related_constituents(directory, graphs):
     def run_query(graph):
-        # TODO: write query that will inject triples
-        """
-        @return: table or pandas dataframe
-        """
+        query = """
+                    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                    PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
 
-        q = """
-        SELECT * WHERE
-        {
-            ?s ?p ?o .
-        } LIMIT 10
-        """
-    pass
+                    SELECT ?constituent ?object
+                    WHERE{
+                        ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/ConstituentID> ?constituent .
+                        ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/RoleTypeID> ?RoleTypeID .
+                        ?conxrefdetailID <https://pressingmatter.nl/Bronbeek/ConXrefDetails/vocab/ConXrefID> ?conxref .
+                        ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/ID> ?ID .
+                        ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/TableID> ?TableID .
+                        FILTER (?TableID = "108"^^<https://pressingmatter.nl/Bronbeek/ConXrefs/interger>) .
+                        ?conXref <https://pressingmatter.nl/Bronbeek/ConXrefs/vocab/RoleTypeID> <https://pressingmatter.nl/Bronbeek/RoleTypes/1> .
+                        ?object <https://pressingmatter.nl/Bronbeek/Objects/vocab/ObjectID> ?ID .
+                    }
+                """
+
+        result = graph.query(query)
+
+        rows = []
+        for row in result:
+            constituent, object = row
+            rows.append({
+                'constituent': str(constituent),
+                'object': str(object)
+            })
+        df = pandas.DataFrame(rows)
+
+        return df
+
+    def insert_links(df):
+        g = Graph()
+        crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+        g.bind("crm", crm)
+
+        try:
+            for i, row in tqdm(df.iterrows()):
+                constituent = URIRef(str(row.iloc[0]).strip().replace('"', ''))
+                object = URIRef(str(row.iloc[1]).strip().replace('"', ''))
+
+                g.add((object, SKOS.related, constituent))
+        finally:
+            print(len(g))
+            return g
+
+    insert_links(run_query(graphs)) \
+        .serialize(destination=os.path.join(directory, 'person_related.ttl'))
 
 
 def enrich_data(directory):
@@ -149,6 +214,8 @@ def enrich_data(directory):
     print(len(all_graphs))
 
     add_acquisition_event(directory, all_graphs)
+    add_former_owner(directory, all_graphs)
+    add_related_constituents(directory, all_graphs)
 
 
 if __name__ == "__main__":
